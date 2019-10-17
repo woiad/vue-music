a26cffa8d2fddec25b11d839634ee394a15e7524
woiad <934480653@qq.com>
2019-10-17 16:42:19 +0800

    歌手页开发完成,w

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -33,14 +33,22 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{ fixedTitle }}</h1>
+    </div>
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
 import { getData } from 'common/js/dom'
+import Loading from 'base/loading/loading'
 
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 export default {
   name: 'Listview',
   props: {
@@ -54,23 +62,32 @@ export default {
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   computed: {
     shortcutList() {
       return this.data.map(group => {
         return group.title.substr(0, 1)
       })
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   created() {
     this.touch = {}
     this.listenScroll = true
     this.listHeigth = []
+    this.fixedTop = 0
   },
   watch: {
     data() {
@@ -90,11 +107,20 @@ export default {
         let height2 = listHeight[i + 1]
         if (!height2 || (-newY >= height1 && -newY < height2)) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return true
         }
       }
       // 当滚到到底部,且-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return true
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
     }
   },
   methods: {
