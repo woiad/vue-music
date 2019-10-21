a58a666e7001fe9ecd60c3476568f2ef9bd7a1fd
woiad <934480653@qq.com>
2019-10-21 11:36:35 +0800

    vuex初始化

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -5,8 +5,17 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'SingerDetail'
+  name: 'SingerDetail',
+  computed: {
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  created() {
+    console.log(this.singer)
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import fastclick from 'fastclick' // 解决移动端点击事件300毫秒延迟
 import 'common/stylus/index.styl'
 import VueLazyLoad from 'vue-lazyload'
@@ -14,5 +15,6 @@ fastclick.attach(document.body)
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
